a5f89be03ea1d72ddffa4fa379ed9a1f09b551de
Jeet Narkhede <jeetnarkhede@gmail.com>
2025-10-14 23:07:31 -0400

    a3_final_v6

--- app/Labs/Lab3/todos/TodoList.tsx
+++ app/Labs/Lab3/todos/TodoList.tsx
@@ -1,13 +1,17 @@
 import ListGroup from "react-bootstrap/esm/ListGroup";
 import TodoItem from "./TodoItem";
 import todos from "./todos.json";
+
 export default function TodoList() {
- return(
-   <>
-     <h3>Todo List</h3>
-     <ListGroup>
-       { todos.map(todo => {
-           return(<TodoItem key={todo.id} todo={todo}/>);   })}
-     </ListGroup><hr/>
-   </>
-);}
+  return (
+    <>
+      <h3>Todo List</h3>
+      <ListGroup>
+        {todos.map((todo, index) => (
+          <TodoItem key={index} todo={todo} />
+        ))}
+      </ListGroup>
+      <hr />
+    </>
+  );
+}
