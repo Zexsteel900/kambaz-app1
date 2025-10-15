75713e2a4ff4a3039397f0c59625936c3064db41
Jeet Narkhede <jeetnarkhede@gmail.com>
2025-10-14 23:00:22 -0400

    a3_final_v4

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -10,9 +10,11 @@ import { useParams } from "next/navigation";
 export default function Modules() {
   const params = useParams();
   const cid = params?.cid;
-  const courseModules = modules.filter(m => m.course === cid);
 
   if (!cid) return <div>No course selected</div>;
+
+  const courseModules = modules.filter(m => m.course === cid);
+
   if (courseModules.length === 0) return <div>No modules found for this course</div>;
 
   return (
@@ -21,23 +23,26 @@ export default function Modules() {
         <div className="flex-fill me-3">
           <ModulesControls /><br /><br /><br /><br />
           <ListGroup className="rounded-0" id="wd-modules-list">
-            {courseModules.map((module) => (
-              <ListGroupItem key={module._id} className="wd-module p-0 mb-5 fs-5 border-gray">
-                <div className="wd-title p-3 ps-2 bg-secondary">
-                  <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
-                </div>
-                <ListGroup className="wd-lessons rounded-0">
-                  {module.lessons?.length > 0
-                    ? module.lessons.map(lesson => (
-                        <ListGroupItem key={lesson._id} className="wd-lesson p-3 ps-1">
-                          <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
-                        </ListGroupItem>
-                      ))
-                    : <ListGroupItem className="wd-lesson p-3 ps-1">No lessons for this module</ListGroupItem>
-                  }
-                </ListGroup>
-              </ListGroupItem>
-            ))}
+            {courseModules.map((module) => {
+              const lessons = module.lessons ?? []; // default to empty array
+              return (
+                <ListGroupItem key={module._id} className="wd-module p-0 mb-5 fs-5 border-gray">
+                  <div className="wd-title p-3 ps-2 bg-secondary">
+                    <BsGripVertical className="me-2 fs-3" /> {module.name} <ModuleControlButtons />
+                  </div>
+                  <ListGroup className="wd-lessons rounded-0">
+                    {lessons.length > 0
+                      ? lessons.map((lesson) => (
+                          <ListGroupItem key={lesson._id} className="wd-lesson p-3 ps-1">
+                            <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+                          </ListGroupItem>
+                        ))
+                      : <ListGroupItem className="wd-lesson p-3 ps-1">No lessons for this module</ListGroupItem>
+                    }
+                  </ListGroup>
+                </ListGroupItem>
+              );
+            })}
           </ListGroup>
         </div>
       </div>
--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -4,6 +4,24 @@ import { FaUserCircle } from "react-icons/fa";
 import { useParams } from "next/navigation";
 import * as db from "../../../../Database";
 
+// Define TypeScript interfaces
+interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  loginId: string;
+  section: string;
+  role: string;
+  lastActivity: string;
+  totalActivity: string;
+}
+
+interface Enrollment {
+  _id: string;
+  user: string;
+  course: string;
+}
+
 export default function PeopleTable() {
   const { cid } = useParams();
   const { users, enrollments } = db;
@@ -12,14 +30,21 @@ export default function PeopleTable() {
   const courseIdMap: Record<string, string> = {
     "1234": "RS101",
     "5678": "RS102",
-    "91011": "RS103"
+    "91011": "RS103",
   };
 
-  const actualCourseId = courseIdMap[cid];
+  const actualCourseId = courseIdMap[cid as string];
+
+  if (!actualCourseId) {
+    return <p>Invalid course ID.</p>;
+  }
 
   // Filter users enrolled in the current course
-  const courseUsers = users.filter(user =>
-    enrollments.some(enrollment => enrollment.user === user._id && enrollment.course === actualCourseId)
+  const courseUsers = (users as User[]).filter((user) =>
+    (enrollments as Enrollment[]).some(
+      (enrollment) =>
+        enrollment.user === user._id && enrollment.course === actualCourseId
+    )
   );
 
   if (courseUsers.length === 0) {
@@ -28,8 +53,8 @@ export default function PeopleTable() {
 
   return (
     <div id="wd-people-table">
-      <table className="table table-striped">
-        <thead>
+      <table className="table table-bordered table-hover align-middle">
+        <thead className="table-light">
           <tr>
             <th>Full Name</th>
             <th>Login ID</th>
@@ -40,7 +65,7 @@ export default function PeopleTable() {
           </tr>
         </thead>
         <tbody>
-          {courseUsers.map(user => (
+          {courseUsers.map((user) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
                 <FaUserCircle className="me-2 fs-1 text-secondary" />
