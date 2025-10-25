dbf468ecc512b3c7a1cee1ee9973a80e88a8d741
Jeet Narkhede <jeetnarkhede@gmail.com>
2025-10-25 17:10:09 -0400

    a4 Redux_done_final_v3

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Provider } from "react-redux";
-import store from "../store";
 import dynamic from "next/dynamic";
 
 const HelloRedux = dynamic(() => import("./HelloRedux/page"), { ssr: false });
@@ -10,14 +8,12 @@ const TodosRedux = dynamic(() => import("./todos/page"), { ssr: false });
 
 export default function ReduxExamples() {
     return (
-        <Provider store={store}>
-            <div>
-                <h2> Redux Examples </h2>
-                <HelloRedux />
-                <CounterRedux />
-                <AddRedux />
-                <TodosRedux />
-            </div>
-        </Provider>
+        <div>
+            <h2> Redux Examples </h2>
+            <HelloRedux />
+            <CounterRedux />
+            <AddRedux />
+            <TodosRedux />
+        </div>
     )
 }
